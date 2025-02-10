daf3be6c54e71fe210ce4b2ff67edbb3376473bc
Hannah Mohd-Rawi <85315928+hannahmohdrawi@users.noreply.github.com>
2025-02-10 21:53:14 +0000

    Shopping Cart functions

--- index.js
+++ index.js
@@ -18,3 +18,32 @@ function nextSlide() {
     currentIndex = (currentIndex + 1) % banners.length;
     showSlide(currentIndex);
 }
+
+
+//Shopping Cart 
+let cart = [];
+
+function toggleCart(){
+    document.getElementById('cartPopup').classList.toggle('open');
+}
+
+function addToCart(itemName, itemPrice){
+    cart.push({name: itemName, price: itemPrice});
+    updateCartDisplay();
+}
+
+function updateCartDisplay(){
+    const cartItemsContainer = document.getElementById('cartItems');
+    const cartTotalContainer = document.getElementById('cartTotal');
+
+    cartItemsContainer.innerHTML = '';
+    let total = 0;
+
+    cart.forEach(item => {
+        total += item.price;
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `${item.name} - £${item.price.toFixed(2)} `;
+        cartItemsContainer.appendChild(listItem);
+    });
+    cartTotalContainer.innerText = total.toFixed(2);
+}
